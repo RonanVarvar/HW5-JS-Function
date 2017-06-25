526897abddcadbdeb00fafce3e0f3b328e37d9a5
Roman Veselovsky <romanveselovsky@gmail.com>
2017-06-25 10:33:16 +0300

    HW5-JS-Function: fixed script.js

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ function isSmaller(a, b) {
 console.log(isSmaller(5, -1));
 
 function getMin() {
-    var min = arguments[0];
-    for (var i = 0; i < arguments.length; i++) {
-        if (min > arguments[i]) min = arguments[i];
+    
+	var min = arguments[0];
+    
+	for (var i = 0; i < arguments.length; i++) {
+		if (min > arguments[i]) { 
+			min = arguments[i];
+		}
     }
+	
     return min;
 }
 console.log(getMin(3, 0, -3));
@@ -24,10 +29,15 @@ function pow(x, n) {
 console.log(pow(2, 3));
 
 function getClosestToZero() {
-    var toZero = arguments[0];
-    for (var i = 0; i < arguments.length; i++) {
-        if (toZero > Math.abs(arguments[i])) toZero = arguments[i];
+	
+	var toZero = arguments[0];
+    
+	for (var i = 0; i < arguments.length; i++) {
+        if (toZero > Math.abs(arguments[i])) {
+			toZero = arguments[i];
+		}
     }
+	
     return toZero;
 }
-console.log(getClosestToZero(7, 2, 5, -1, -9));
+console.log(getClosestToZero(9, 5, -4, -9));
